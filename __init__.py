09ddc3fd5d752bc909d838611a57455e05a048ec
Mingxu Hu <humingxu@gmail.com>
2024-05-25 20:10:30 +0800

    cleanup

--- __init__.py
+++ __init__.py
@@ -1,8 +1,5 @@
 __all__ = [
     'get_sym_grp',
-    'meanvar_SO3_G',
-    'meanvar_S2_G',
-    'solve_NUG',
     'averaging_SO3',
     'averaging_S2',
     'averaging_SO3_G',
@@ -10,8 +7,7 @@ __all__ = [
 ]
 
 from .symmetry_group import get_sym_grp
-from .meanvar import meanvar_S2_G
-from .NUG import solve_NUG
+# from .NUG import solve_NUG
 
 from .averaging_SO3 import mean_SO3, variance_SO3
 from .averaging_S2  import mean_S2 , variance_S2
