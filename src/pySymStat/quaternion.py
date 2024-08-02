0ec2d0145a002ab74f8f24e58e6c9d63ccb159fe
Mingxu Hu <humingxu@smart.org.cn>
2024-08-02 17:26:28 +0800

    quaternion updated

--- src/pySymStat/quaternion.py
+++ src/pySymStat/quaternion.py
@@ -8,13 +8,18 @@ __all__ = [
 
 import numpy as np
 
-def quat_norm(q):
+from numpy.typing import NDArray
+
+def quat_norm(q : NDArray[np.float64]) -> float:
     return np.linalg.norm(q)
 
-def quat_conj(q):
+def quat_conj(q : NDArray[np.float64]) -> NDArray[np.float64]:
     return q * np.array([1, -1, -1, -1])
 
-def quat_mult(q1, q2):
+def quat_mult(
+    q1 : NDArray[np.float64],
+    q2 : NDArray[np.float64]
+) -> NDArray[np.float64]:
     w1, x1, y1, z1 = q1
     w2, x2, y2, z2 = q2
     return np.array([w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2,
@@ -22,10 +27,17 @@ def quat_mult(q1, q2):
                      w1 * y2 - x1 * z2 + y1 * w2 + z1 * x2,
                      w1 * z2 + x1 * y2 - y1 * x2 + z1 * w2], dtype = np.float64)
 
-def quat_same(q1, q2, eps = 1e-3):
+def quat_same(
+    q1 : NDArray[np.float64],
+    q2 : NDArray[np.float64],
+    eps : float = 1e-3
+) -> bool:
     return np.linalg.norm(q1 - q2) < eps or np.linalg.norm(q1 + q2) < eps
 
-def quat_rotate(q, v):
+def quat_rotate(
+    q : NDArray[np.float64], 
+    v : NDArray[np.float64]
+) -> NDArray[np.float64]:
     v = np.array([0, v[0], v[1], v[2]], dtype = np.float64)
     qv = quat_mult(quat_mult(q, v), quat_conj(q))
     return qv[1:]
